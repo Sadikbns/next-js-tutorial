0a80f93b6c6739a5eb28e4df87349e20833f551f
Elseddik Bensadi <sadikbensadi7@gmail.com>
2025-12-09 08:45:33 +0100

    layout file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,19 @@
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang="en">
+      <body>
+        <header style={{width: '100%', backgroundColor: 'blueviolet', padding: '1rem'}}>
+          <p>
+              Header
+          </p>
+        </header>
+        {children}
+        <footer style={{width: '100%', backgroundColor: 'red', padding: '1rem'}}>
+          <p>
+              Footer
+          </p>
+        </footer>
+      </body>
     </html>
   )
 }
